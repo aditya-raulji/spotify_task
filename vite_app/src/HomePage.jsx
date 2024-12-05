8b7500f90719955641fdbc79f082b6d29d94df14
aditya-raulji <-email@example.com>
2024-12-05 12:12:42 +0530

    update

---
+++ vite_app/src/HomePage.jsx
@@ -0,0 +1,170 @@
+import './style.css'
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
+function Homepage() {
+
+  return (
+    <>
+
+      <div className="container">
+        <img src="https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/10.spotify-clone/assets/Spotify%20logo.png"
+          alt="" className="logo" />
+        <div className='aditya'><img className="icons"
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Home.png?raw=true"
+          alt="" />
+          <p className="text">Home</p>
+        </div>
+      </div>
+      <div className='raulji'><img className="icons"
+        src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Search.png?raw=true"
+        alt="" />
+        <p className="text">search</p>
+      </div>
+      <div className='abc'><img class="icons"
+        src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Your%20library.png?raw=true"
+        alt="" />
+        <p className="text">Your Library</p>
+      </div>
+
+
+      <div className='new1'><img className="icons"
+        src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Create.png?raw=true"
+        alt="" />
+        <p className="text">Create playlist</p>
+      </div>
+
+      <div className='new2'><img className="icons"
+        src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Liked.png?raw=true"
+        alt="" />
+        <p className="text">Liked Songs</p>
+      </div>
+
+
+      <div className='new3'><img className="icons"
+        src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/your%20episodes.png?raw=true"
+        alt="" />
+        <p className="text">Your Episodes</p>
+      </div>
+
+      <div>
+        <p className='new4'>FAV</p>
+      </div>
+      <div>
+        <p className='new5'>Daily Mix 1</p>
+      </div>
+      <div>
+        <p className='new6'>Discover weekly</p>
+      </div>
+      <div>
+        <p className='new7'>Malayalam</p>
+      </div>
+      <div>
+        <p className='new8'>Dance / Electronix mix</p>
+      </div>
+      <div>
+        <p className='new9'>EDM / Popular</p>
+      </div>
+      <div>
+        <img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/grommet-icons_install-option.png?raw=true"
+          alt="" className="icons new11" />
+        <p className='new12'>Install App</p>
+      </div>
+      <div className="container2">
+        <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/User.png?raw=true"
+          alt="" class="images" />
+        <img className="arrows"
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/top%20arrow%20navigation.png?raw=true"
+          alt="" />
+        <p className="Good">Good Morning</p>
+      </div>
+      <div className="container3">
+        <div className="child">
+          <img className="images2"
+            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Liked.png?raw=true"
+            alt="" />
+          <p className="text2">Liked Songs</p>
+        </div>
+        <div className="child">
+          <img className="images2"
+            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image4.png?raw=true"
+            alt="" />
+          <p className="text2">Neffex Playlist</p>
+        </div>
+        <div className="child">
+          <img className="images2"
+            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image3.png?raw=true"
+            alt="" />
+          <p className="text2">K/DA</p>
+        </div>
+        <div className="child">
+          <img className="images2"
+            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image1.png?raw=true"
+            alt="" />
+          <p className="text2">Liked Songs</p>
+        </div>
+        <div className="child">
+          <img className="images2"
+            src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image2.png?raw=true"
+            alt="" />
+          <p className="text2">Dance/ Electronix mix</p>
+        </div>
+      </div>
+      <p className="Shows">Shows You Might Like</p>
+      <div className="container4">
+        <div className="child2">
+          <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image9.png?raw=true"
+            alt="" className="images3" />
+          <p className="text3">Weekly Motivatio...</p>
+        </div>
+        <div className="child2"><img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image8.png?raw=true"
+          alt="" className="images3" />
+          <p className="text3">Meditation SELF</p>
+        </div>
+        <div className="child2"><img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image7.png?raw=true"
+          alt="" className="images3" />
+          <p className="text3">Words beyond act... </p>
+        </div>
+        <div className="child2"><img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image6.png?raw=true"
+          alt="" className="images3" />
+          <p className="text3">The Alexa Shows</p>
+        </div>
+        <div className="child2"><img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image5.png?raw=true"
+          alt="" className="images3" />
+          <p className="text3">The Stories of Ma...</p>
+        </div>
+        <div className="child2"><img
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image10.png?raw=true"
+          alt="" className="images3" />
+          <p className="text3">Motivation Daily b...</p>
+        </div>
+      </div>
+
+      <div className="container5">
+        <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/image11.png?raw=true"
+          alt="" className="images4" />
+        <p className="new19" 
+    >Dreaming On <br />NEFEX</p>
+        <img className='new20'
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/bx_bxs-heart.png?raw=true"
+          alt="" />
+        <img className="images5"
+          src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Music%20Player%20Options.png?raw=true"
+          alt="" />
+        <img src="https://github.com/PatelNeelMahesh/frontend_tasks/blob/main/10.spotify-clone/assets/Right%20options.png?raw=true"
+          alt="" className="images6" />
+      </div>
+      <div className='new22'><p className='new50'>Listning on ASUS-ROG-G531GT</p> </div>
+
+
+    </>
+  )
+}
+
+export default Homepage
